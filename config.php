be0796d7ba7fb5dfa7d911925e5f503476c58743
Harun Selimanovic <harun11selimanovic@gmail.com>
2019-04-08 11:43:56 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
-<?php return array (
-  'db' => 
-  array (
-    'host' => 'localhost',
-    'port' => '3306',
-    'username' => 'root',
-    'password' => 'root',
-    'dbname' => 'bootcamp',
-  ),
-);
+<?php
+return [
+    'db' => [
+        'username' => '%db.user%',
+        'password' => '%db.password%',
+        'dbname' => '%db.database%',
+        'host' => '%db.host%',
+        'port' => '%db.port%'
+    ]
+];
