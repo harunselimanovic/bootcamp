94e0b8561c8d2f66f4771538ff673ef7611f3834
hdurak <harun_d98@hotmail.com>
2019-04-09 17:03:40 +0200

    finalne promjene

--- config.php
+++ config.php
@@ -1,10 +1,31 @@
-<?php return array (
+<?php return [
   'db' => 
-  array (
+  [
     'host' => 'localhost',
     'port' => '3306',
     'username' => 'root',
     'password' => '',
     'dbname' => 'bootcamp',
-  ),
-);
+  ],
+    'front' => [
+        'throwExceptions' => true,
+        'showException' => true
+    ],
+
+    'phpsettings' => [
+        'display_errors' => 1
+    ],
+
+    'template' => [
+        'forceCompile' => true
+    ],
+
+    'csrfProtection' => [
+        'frontend' => true,
+        'backend' => true
+    ],
+
+    'httpcache' => [
+        'debug' => true
+    ]
+];
